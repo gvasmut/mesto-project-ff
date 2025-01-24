eee2ccca28bc817883b695407b1dd38e67b202ff
gvasmut <g.vasmut@gmail.com>
2025-01-24 17:20:28 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/gvasmut/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function addCard(cardData) {
+  let cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  deleteButton.addEventListener("click", function () {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  cardElement = addCard(cardData);
+  cardsList.append(cardElement);
+});
