e48e7786d82cc4a0a424777e7d7f111054ba85dc
gvasmut <g.vasmut@gmail.com>
2025-01-26 20:24:08 +0200

    fixed reviewer's comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,27 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-const cardsList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function addCard(cardData) {
-  let cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
-  cardElement.querySelector(".card__image").src = cardData.link;
+function createCard(cardData, cardDelete) {
+  const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+
   cardElement.querySelector(".card__title").textContent = cardData.name;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  deleteButton.addEventListener("click", function () {
-    cardElement.remove();
-  });
-
+  deleteButton.addEventListener("click", () => cardDelete(cardElement));
+  
   return cardElement;
 }
 
+function cardDelete(cardElement) {
+  cardElement.remove();
+}
+
 initialCards.forEach((cardData) => {
-  cardElement = addCard(cardData);
-  cardsList.append(cardElement);
+  const cardElement = createCard(cardData, cardDelete);
+  cardsContainer.append(cardElement);
 });
