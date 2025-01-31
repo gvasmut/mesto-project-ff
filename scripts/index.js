144a8e2550a709e982f8a5c2c47d9cb7d771b33b
gvasmut <g.vasmut@gmail.com>
2025-01-31 17:23:13 +0200

    function name was fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const cardsContainer = document.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function createCard(cardData, cardDelete) {
+function createCard(cardData, deleteCard) {
   const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
 
@@ -12,16 +12,16 @@ function createCard(cardData, cardDelete) {
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
-  deleteButton.addEventListener("click", () => cardDelete(cardElement));
-  
+  deleteButton.addEventListener("click", () => deleteCard(cardElement));
+
   return cardElement;
 }
 
-function cardDelete(cardElement) {
+function deleteCard(cardElement) {
   cardElement.remove();
 }
 
 initialCards.forEach((cardData) => {
-  const cardElement = createCard(cardData, cardDelete);
+  const cardElement = createCard(cardData, deleteCard);
   cardsContainer.append(cardElement);
 });
